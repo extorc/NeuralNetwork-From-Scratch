804ec6b11958c95e7d31c65a975753cae6901d4a
extorc <mittalshaurya004@gmail.com>
2021-07-07 10:21:38 +0530

    Variable Renaming

--- nn.py
+++ nn.py
@@ -2,29 +2,30 @@ from data import get_mnist
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 """
 w = weights, b = bias, i = input, h = hidden, o = output, l = label
-e.g. w_i_h = weights from input layer to hidden layer
+e.g. l1_weights = weights from input layer to hidden layer
 """
+
 images, labels = get_mnist()
-w_i_h = np.random.uniform(-0.5, 0.5, (20, 784))
-w_h_o = np.random.uniform(-0.5, 0.5, (10, 20))
-b_i_h = np.zeros((20, 1))
-b_h_o = np.zeros((10, 1))
+l1_weights = np.random.uniform(-0.5, 0.5, (20, 784))
+l2_weights = np.random.uniform(-0.5, 0.5, (10, 20))
+l1_bias = np.zeros((20, 1))
+l2_bias = np.zeros((10, 1))
 
 learn_rate = 0.01
 nr_correct = 0
 epochs = 3
+
 for epoch in range(epochs):
     for img, l in zip(images, labels):
         img.shape += (1,)
         l.shape += (1,)
         # Forward propagation input -> hidden
-        h_pre = b_i_h + w_i_h @ img
+        h_pre = l1_bias + l1_weights @ img
         h = 1 / (1 + np.exp(-h_pre))
         # Forward propagation hidden -> output
-        o_pre = b_h_o + w_h_o @ h
+        o_pre = l2_bias + l2_weights @ h
         o = 1 / (1 + np.exp(-o_pre))
 
         # Cost / Error calculation
@@ -33,12 +34,12 @@ for epoch in range(epochs):
 
         # Backpropagation output -> hidden (cost function derivative)
         delta_o = o - l
-        w_h_o += -learn_rate * delta_o @ np.transpose(h)
-        b_h_o += -learn_rate * delta_o
+        l2_weights += -learn_rate * delta_o @ np.transpose(h)
+        l2_bias += -learn_rate * delta_o
         # Backpropagation hidden -> input (activation function derivative)
-        delta_h = np.transpose(w_h_o) @ delta_o * (h * (1 - h))
-        w_i_h += -learn_rate * delta_h @ np.transpose(img)
-        b_i_h += -learn_rate * delta_h
+        delta_h = np.transpose(l2_weights) @ delta_o * (h * (1 - h))
+        l1_weights += -learn_rate * delta_h @ np.transpose(img)
+        l1_bias += -learn_rate * delta_h
 
     # Show accuracy for this epoch
     print(f"Acc: {round((nr_correct / images.shape[0]) * 100, 2)}%")
@@ -52,11 +53,11 @@ while True:
 
     img.shape += (1,)
     # Forward propagation input -> hidden
-    h_pre = b_i_h + w_i_h @ img.reshape(784, 1)
+    h_pre = l1_bias + l1_weights @ img.reshape(784, 1)
     h = 1 / (1 + np.exp(-h_pre))
     # Forward propagation hidden -> output
-    o_pre = b_h_o + w_h_o @ h
+    o_pre = l2_bias + l2_weights @ h
     o = 1 / (1 + np.exp(-o_pre))
 
-    plt.title(f"Subscribe if its a {o.argmax()} :)")
+    print(o.argmax())
     plt.show()
