544ae417011ddeee18f52b6ec59d7de3adc7c5d0
extorc <mittalshaurya004@gmail.com>
2021-07-16 17:13:39 +0530

    Simplification

--- data.py
+++ data.py
@@ -1,9 +1,7 @@
 import numpy as np
-import pathlib
-
 
 def get_mnist():
-    with np.load(f"{pathlib.Path(__file__).parent.absolute()}/data/mnist.npz") as f:
+    with np.load(f"data/mnist.npz") as f:
         images, labels = f["x_train"], f["y_train"]
     images = images.astype("float32") / 255
     images = np.reshape(images, (images.shape[0], images.shape[1] * images.shape[2]))
